1c5846bbdab9be9ab649d14273dda4c1941296cb
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-06 16:17:47 -0700

    Created functions to allow user to select the color. Requires addition field on the grid object for the color, a function to convert the hex value of the colorPicker input to rgb, which then updates the grid objects color value. Also required updating the countHover function to extract the number values of the grid.color rgba string to then update the color that is applied when a cell is hovered.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+let c = {
+    r: 0,
+    g: 0,
+    b: 0
+}
+
 let grid = {
     cols: 16,
     rows: 16,
+    color: `rgba(0, 0, 0, 0)`,
     matrix: [],
     clear: false
 }
@@ -21,15 +28,6 @@ function makeGrid(cols, rows) {
         column.push(row)
     }
     return column
-}
-
-function countHover(cell) {
-    let count = parseInt(cell.getAttribute('data-count'))
-    count++
-    if (count < 9) {
-        cell.style.backgroundColor = `rgba(0,0,0,0.${count})`
-        cell.setAttribute('data-count', `${count}`)
-    }
 }
 
 function displayMatrix() {
@@ -54,6 +52,7 @@ function displayMatrix() {
     displayGrid.appendChild(container)
 }
 
+// INPUT ELEMENTS, BUTTONS & ATTRIBUTES
 let slider = document.createElement('input')
 slider.setAttribute('type', 'range')
 slider.setAttribute('id', 'slider')
@@ -64,11 +63,19 @@ let clearMatrixBtn = document.createElement('button')
 let drawMatrixBtn = document.createElement('button')
 clearMatrixBtn.innerText = "Clear"
 drawMatrixBtn.innerText = "Draw Matrix"
+let colorPicker = document.createElement('input')
+colorPicker.setAttribute('type', 'color')
+colorPicker.setAttribute('id', 'color')
+colorPicker.setAttribute('value', 'rgba(0,0,0,0)')
 
 // EVENT LISTENERS
 // --- get the value of the slider to set the grid size
 slider.oninput = function() {
     setGridSize()
+}
+// --- get the value of the colorPicker to set the color
+colorPicker.oninput = function() {
+    setColor()
 }
 // --- erase the entire board
 clearMatrixBtn.addEventListener('click', () => {
@@ -95,9 +102,49 @@ function setGridSize() {
     grid.cols = val
     grid.rows = val
 }
+// --- set the color of the grid object that will be drawn when hovered
+function setColor() {
+    let val = document.getElementById('color').value
+    let rgba = hexToRgbA(val)
+    grid.color = rgba
+}
+// --- hover the cell to draw
+//     + will track the amount of times to darken the cell,
+//       as well as track the selected color
+function countHover(cell) {
+    let count = parseInt(cell.getAttribute('data-count'))
+
+    // --- extract values from grid.color
+    let { color } = grid
+    let values = color.substring(5, color.length - 1).split(',')
+
+    // --- add to how many times a cell has been hovered to shade the cell
+    count++
+    if (count < 9) {
+        cell.style.backgroundColor = `rgba(${values[0]},${values[1]},${values[2]},0.${count})`
+        cell.setAttribute('data-count', `${count}`)
+    }
+}
+
+// HELPER FUNCTIONS
+// --- convert hex value from colorPicker to rgb
+//     @RESOURCE: https://stackoverflow.com/questions/21646738/convert-hex-to-rgba
+function hexToRgbA(hex) {
+    let c
+    if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+        c = hex.substring(1).split('')
+        if (c.length === 3) {
+            c = [c[0], c[0], c[1], c[1], c[2], c[2]]
+        }
+        c = '0x' + c.join('')
+        return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',0)'
+    }
+    throw new Error('Bad Hex')
+}
 
 displayMatrix()
 
 inputSection.appendChild(slider)
 inputSection.appendChild(clearMatrixBtn)
 inputSection.appendChild(drawMatrixBtn)
+inputSection.appendChild(colorPicker)
