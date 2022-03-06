cbb560f9a6741602a32a86fbca898aeead5aeed1
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-06 04:00:35 -0700

    Added hover effect to grid item

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let grid = {
     matrix: []
 }
 
+let displayGrid = document.querySelector('.display-grid')
+
+
 function makeGrid() {
     let column = []
     for (let c = 0; c < grid.cols; c++) {
@@ -21,11 +24,16 @@ function makeGrid() {
 function displayMatrix(m) {
     let container = document.createElement('div')
     container.classList.add('matrix', 'parent')
-
-    m.map(el => {
-        console.log(el)
+    container.style.setProperty('--grid-rows', grid.rows)
+    container.style.setProperty('--grid-cols', grid.cols)
+    m.forEach(el => {
+        el.forEach(ce => {
+            container.appendChild(ce)
+        })
     })
+    displayGrid.appendChild(container)
 }
 
 grid.matrix = makeGrid()
+
 displayMatrix(grid.matrix)
